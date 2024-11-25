32684c537c1ec755125f03ed85a5a9ed0a288e97
Hieu <hieutrungpham91@gmail.com>
2024-11-25 14:32:58 +0200

    add useEffect to fetch initial data from json server

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {setPersons(response.data)})
+    }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -23,7 +23,7 @@ const App = () => {
       number: newNumber
     }
 
-    if (persons.map(person => person.name.toLocaleLowerCase()).includes(newName.toLocaleLowerCase())) {
+    if (persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())) {
       alert(`${newName} is already added to the phonebook`)
     }
     else {
@@ -43,7 +43,7 @@ const App = () => {
 
   const personToShow = showAll
     ? persons
-    : persons.filter(person => person.name.toLowerCase() === filterName.toLocaleLowerCase())
+    : persons.filter(person => person.name.toLowerCase() === filterName.toLowerCase())
 
   const handleFilter = (event) => {
     setFilterName(event.target.value)
