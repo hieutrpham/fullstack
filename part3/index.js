3213829abec4ace6c543685dbcffd09f7e954bc4
Hieu <hieutrungpham91@gmail.com>
2024-11-30 09:44:16 +0200

    connecting front to backend

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
