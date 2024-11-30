b699c61ef525e06f16be2e3ec7012f88d93cd0c4
Hieu <hieutrungpham91@gmail.com>
2024-11-30 07:24:47 +0200

    middleware + handling requests

--- part3/index.js
+++ part3/index.js
@@ -3,6 +3,20 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 let notes = [
   {
     id: "1",
@@ -72,6 +86,9 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+app.use(unknownEndpoint)
+
   
 const PORT = 3001
 app.listen(PORT, () => {
