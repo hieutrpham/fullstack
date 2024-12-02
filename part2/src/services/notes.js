2dba8820331d3f5126114691e1b9cfc8f3303b08
Hieu <hieutrungpham91@gmail.com>
2024-12-02 14:37:46 +0200

    update mongo

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
+    console.log(request);
+    
     return request.then(response => response.data)
 }
 
