3ac69656af2d118b750297046d030ce617c747b3
Hieu <hieutrungpham91@gmail.com>
2024-12-02 18:25:01 +0200

    error handling

--- part3/index.js
+++ part3/index.js
@@ -4,8 +4,6 @@ import cors from 'cors'
 import Note from './models/note.js'
 import mongoose from 'mongoose'
 
-const Notes = mongoose.model('Note', Note)
-
 const app = express()
 
 app.use(express.json())
@@ -21,7 +19,19 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+
 app.use(requestLogger)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+
+const Notes = mongoose.model('Note', Note)
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -36,17 +46,18 @@ app.get('/api/notes', (request, response) => {
   Notes.find({}).then(notes => {response.json(notes)})
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {  
   Notes.findById(request.params.id).then(note => {
-    response.json(note)
+    if (note) {response.json(note)}
+    else {response.status(404).end()}
   })
+  .catch(err => next(err))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Notes.findByIdAndDelete(request.params.id)
+    .then(result => {response.status(204).end()})
+    .catch(err => next(err))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -69,8 +80,21 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.use(unknownEndpoint)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Notes.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {response.json(updatedNote)})
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
