c67ed384d25e74915df58be6d6825959fd118a5d
Hieu <hieutrungpham91@gmail.com>
2024-12-23 15:07:29 +0200

    Revert "ex 6.9"
    This reverts commit 1fcc058cda273460c33f01b7d0e92c35cd22c6ea.

--- redux-notes/src/App.js
+++ redux-notes/src/App.js
@@ -1,13 +1,10 @@
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
-import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-
   return (
     <div>
       <NewNote />
-      <VisibilityFilter />
       <Notes  />
     </div>
   )
--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-
-import filterReducer from './reducers/filterReducer'
 import noteReducer from './reducers/noteReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-}) 
+const store = createStore(noteReducer)
 
-const store = createStore(reducer)
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
