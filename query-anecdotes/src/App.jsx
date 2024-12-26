7c3ca0054846b11269c1d0c9bdeab9f963b68d10
Hieu <hieutrungpham91@gmail.com>
2024-12-26 09:19:10 +0200

    ex 6.21-22

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,15 +1,33 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import {useQuery, useMutation, useQueryClient} from '@tanstack/react-query'
-import { getAnecdotes } from './services'
+import { getAnecdotes, updateAnecdotes } from './services'
 
 const App = () => {
 
   const queryClient = useQueryClient()
 
+  const updateMutation = useMutation({
+    mutationFn: updateAnecdotes,
+    onSuccess: (newAnecdotes) => {
+      // queryClient.invalidateQueries({queryKey: ['anecdotes']})
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.map(item => 
+        item.id === newAnecdotes.id 
+        ? newAnecdotes
+        : item
+      ))
+    }
+  })
+
+  const handleVote = (anecdote) => {
+    updateMutation.mutate({...anecdote, votes: anecdote.votes + 1})
+  }
+
   const result = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: getAnecdotes
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false
   })
 
   if (result.isLoading) {
@@ -21,10 +39,6 @@ const App = () => {
   }
 
   const anecdotes = result.data 
-
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
 
   return (
     <div>
--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,24 @@
+import {useMutation, useQueryClient} from '@tanstack/react-query'
+import { addAnecdotes } from '../services'
+
 const AnecdoteForm = () => {
+
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: addAnecdotes,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['anecdotes']})
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    console.log(content, content.length)
+    
+    newAnecdoteMutation.mutate({content, votes: 0})
 }
 
   return (
