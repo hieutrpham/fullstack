34b71d5d9837191db89ea642698b84fb0c04371a
Hieu <hieutrungpham91@gmail.com>
2025-01-08 08:17:48 +0200

    part 9b

---
+++ part9/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { calculator, Operation } from "./calculator";
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.post("/calculate", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  res.send({ result });
+});
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
