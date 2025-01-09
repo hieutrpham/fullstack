89b97a5fcbe9bcc81f745a996ca8be4595f7803b
Hieu <hieutrungpham91@gmail.com>
2025-01-09 11:53:53 +0200

    part9c-2

--- part9c-tscript/src/services/diaryService.ts
+++ part9c-tscript/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaryData from "../../data/entries";
-import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from "../types";
 
 const diaries: DiaryEntry[] = diaryData;
 
@@ -7,8 +7,13 @@ const getEntries = (): DiaryEntry[] => {
   return diaries;
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  };
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
@@ -20,8 +25,14 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
+};
+
 export default {
   getEntries,
   addDiary,
+  findById,
   getNonSensitiveEntries,
 };
