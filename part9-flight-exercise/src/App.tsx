4badaadd8a069703daeee855af4aefa02f15d21b
Hieu <hieutrungpham91@gmail.com>
2025-01-12 10:08:54 +0200

    ex 9.19

--- part9-flight-exercise/src/App.tsx
+++ part9-flight-exercise/src/App.tsx
@@ -3,6 +3,7 @@ import { addDiary, getDiaries } from "./service";
 import { DiaryEntry, DiarySchema } from "./types";
 import Diary from "./components/DiaryEntry";
 import DiaryForm from "./components/DiaryForm";
+import { ZodError } from "zod";
 
 const useField = (type: string) => {
   const [value, setValue] = useState("");
@@ -13,6 +14,7 @@ const useField = (type: string) => {
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [noti, setNoti] = useState<string>("");
   const date = useField("date");
   const visibility = useField("text");
   const weather = useField("text");
@@ -24,21 +26,28 @@ const App = () => {
 
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
-    const newEntry = await addDiary(
-      DiarySchema.parse({
-        date: date.value,
-        visibility: visibility.value,
-        weather: weather.value,
-        comment: comment.value,
-      })
-    );
-
-    setDiaries((prev) => prev.concat(newEntry));
+    try {
+      const newEntry = await addDiary(
+        DiarySchema.parse({
+          date: date.value,
+          visibility: visibility.value,
+          weather: weather.value,
+          comment: comment.value,
+        })
+      );
+      setDiaries((prev) => prev.concat(newEntry));
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const notification = err.issues.map((i) => i.message).join("\n");
+        setNoti(notification);
+      }
+    }
   };
 
   return (
     <>
       <h2>Add new entry</h2>
+      <p>{noti}</p>
       <DiaryForm
         onSubmit={handleSubmit}
         date={date.value}
--- part9-flight-exercise/src/service.tsx
+++ part9-flight-exercise/src/service.tsx
@@ -1,15 +1,15 @@
-import axios from 'axios'
-import { DiaryEntry } from './types'
-import { DiaryForm } from './types'
+import axios from "axios";
+import { DiaryEntry } from "./types";
+import { DiaryForm } from "./types";
 
-const baseUrl = 'http://localhost:3000/api/diaries'
+const baseUrl = "http://localhost:3000/api/diaries";
 
 export const getDiaries = async () => {
-  const res = await axios.get<DiaryEntry[]>(baseUrl)
-  return res.data
-}
+  const res = await axios.get<DiaryEntry[]>(baseUrl);
+  return res.data;
+};
 
 export const addDiary = async (newDiary: DiaryForm) => {
-  const res = await axios.post<DiaryForm>(baseUrl, newDiary)
-  return res.data
-}
+  const res = await axios.post<DiaryForm>(baseUrl, newDiary);
+  return res.data;
+};
