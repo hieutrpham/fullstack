3f735eea6a96a3b25825f631f08230432a261ff9
Hieu <hieutrungpham91@gmail.com>
2025-01-15 09:10:47 +0200

    updated to use react-native instead of react-native-web

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,20 +1,15 @@
-import { View, Text } from "react-native-web";
+import { View, Text } from "react-native";
 
 const RepositoryItem = ({ item }) => {
   return (
     <View>
-      <Text>
-        Full Name: {item.fullName} <br />
-        Description: {item.description} <br />
-        Language: {item.language} <br />
-        Forks: {item.forksCount}
-        <br />
-        Stars: {item.stargazersCount}
-        <br />
-        Rating: {item.ratingAverage}
-        <br />
-        Review: {item.reviewCount}
-      </Text>
+      <Text>Full Name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+      <Text>Review: {item.reviewCount}</Text>
     </View>
   );
 };
