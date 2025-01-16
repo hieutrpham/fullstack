0f4764b4d9cdf474d9936d1cc962bba73dbd5c2e
Hieu <hieutrungpham91@gmail.com>
2025-01-16 07:27:22 +0200

    ex 10.5

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, View } from "react-native";
 import AppBar from "./AppBar";
 import RepositoryList from "./RepositoryList";
+import { Route, Routes, Navigate } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: "#e1e4e8",
   },
 });
 
@@ -13,7 +15,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,18 +1,101 @@
-import { View } from "react-native";
+import { Image, View, StyleSheet, Dimensions } from "react-native";
 import Text from "./Text";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+    // flexWrap: "wrap",
+    alignItems: "flex-start",
+  },
+  flexColumn: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 5,
+    maxWidth: "100%",
+  },
+  text: {
+    flexShrink: 1,
+    flexWrap: "wrap",
+    width: "100%",
+    overflow: "hidden",
+  },
+  textStat: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  languageContainer: {
+    backgroundColor: theme.colors.primary,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 12,
+    alignSelf: "flex-start",
+    color: "white",
+    marginTop: 8,
+  },
+});
+
+const formatNumber = (num) => {
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + "k";
+  }
+  return num.toString();
+};
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View>
-      <Text color="primary" fontWeight="bold">
-        Full Name: {item.fullName}
-      </Text>
-      <Text color="textSecondary">Description: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
-      <Text>Review: {item.reviewCount}</Text>
+    <View
+      style={{
+        padding: 1,
+        backgroundColor: "white",
+      }}
+    >
+      <View style={styles.flexRow}>
+        <Image
+          source={{ uri: item.ownerAvatarUrl }}
+          style={{ width: 50, height: 50, marginTop: 10, borderRadius: 7 }}
+        />
+        <View
+          style={[
+            styles.flexColumn,
+            { paddingLeft: 20, flex: 1, flexWrap: "wrap" },
+          ]}
+        >
+          <Text color="primary" fontWeight="bold">
+            {item.fullName} {"\n"}
+          </Text>
+
+          <Text color="textSecondary" style={styles.text}>
+            {item.description} {"\n"}
+          </Text>
+
+          <Text style={styles.languageContainer}>{item.language}</Text>
+        </View>
+      </View>
+      <View style={[styles.flexRow, { justifyContent: "space-around" }]}>
+        <View style={styles.flexColumn}>
+          <Text style={styles.textStat}>{formatNumber(item.forksCount)}</Text>
+          <Text>Forks</Text>
+        </View>
+
+        <View style={styles.flexColumn}>
+          <Text style={styles.textStat}>
+            {formatNumber(item.stargazersCount)}
+          </Text>
+          <Text>Stars</Text>
+        </View>
+
+        <View style={styles.flexColumn}>
+          <Text style={styles.textStat}>{item.reviewCount}</Text>
+          <Text>Reviews</Text>
+        </View>
+
+        <View style={styles.flexColumn}>
+          <Text style={styles.textStat}>{item.ratingAverage}</Text>
+          <Text>Ratings</Text>
+        </View>
+      </View>
     </View>
   );
 };
